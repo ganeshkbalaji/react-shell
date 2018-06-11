4457fdbd2aee40dfb7828c71939e968e66620876
Ganesh Balaji <ganeshkbalaji@ymail.com>
2018-06-10 22:36:40 -0700

    React app skeleton is now up and running

---
+++ app/components/App.js
@@ -0,0 +1,7 @@
+export class MyComponent {
+    render() {
+        return <h1>hello world</h1>
+    }
+}
+
+module.exports = MyComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
 module.exports = {
     entry: __dirname + '/app/index.js',
+    mode: 'development',
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -12,5 +20,6 @@ module.exports = {
     output: {
         filename: 'transformed.js',
         path: __dirname + '/build'
-    }
+    },
+    plugins: [HTMLWebpackPluginConfig]
 };
